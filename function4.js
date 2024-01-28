902e63d3033e218a8920899578854d7f9b5a4c98
grandalf6 <47594725+grandalf6@users.noreply.github.com>
2024-01-28 13:21:56 +0000

    Create function4.js

---
+++ function4.js
@@ -0,0 +1,1 @@
+// Create a function returning UP or DOWN or NULL depending from given temperature. The perfect temperature is 21, so if function receives 20, then should say UP, if 22 then it should say DOWN, if given 21 then should say NULL
