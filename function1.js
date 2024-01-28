2abf5584443f69c1a291f71ee9861d2bf983b4a5
Gabor Varga <varga4x4@outlook.com>
2024-01-28 13:15:00 +0000

    Initial commit

---
+++ function1.js
@@ -0,0 +1,8 @@
+// Johhny has 100 cards, he gives to his younger brother 6 cards every monday, but his mother is giving him 5 cards every wednesday.
+// He also could get 2 cards each time he cleans his room.
+// How many times he had to clean his room in a year to end up with 0 cards.
+
+const function1 = () => {
+    console.log(2)
+}
+function1()
